7f390cfd5b87dd274956e1bc6db3ea36834d7897
MogoBagginz <61120419+MogoBagginz@users.noreply.github.com>
2021-05-09 16:18:56 +0100

    Add files via upload

---
+++ ArrayFiller.java
@@ -0,0 +1,11 @@
+public class ArrayFiller{
+	public static void main(String[] args){
+		int [] listA = [100];
+		int [] listB = [51];
+		int [] listC = [10];
+		int [] listD = [10];
+		int [] listE = [11];
+		
+		for (int i = o; i < nain.length())
+	}
+}
